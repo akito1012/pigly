a314c4d5013cc6ad2a1d690086c9015660e336ac
akito <akito.connected46@gmail.com>
2024-12-06 23:09:46 +0900

    確認５

--- src/resources/views/change.blade.php
+++ src/resources/views/change.blade.php
@@ -8,17 +8,11 @@
 
 <div class="chang">
     <h2 class="change__header">目標体重設定</h2>
-<<<<<<< HEAD
     <form action="/weight_logs/goal_setting" class="change__form" method="post">
         @method('PATCH')
         @csrf
         <div class="change__item">
         <input type="text" class="change input" name="weight" value="{{ old('target_weight') }}" placeholder="46.5">kg
-=======
-    <form action="" class="change__form">
-        <div class="change__item">
-        <input type="text" class="change input" name="weight" placeholder="46.5">kg
->>>>>>> 5892bd1aa6abae8dc313f71a32aef03821ba2dd1
     </div>
     <div class="change__button">
         <button class="back__button" type="submit" value="back">戻る</button>
--- src/resources/views/index/header.blade.php
+++ src/resources/views/index/header.blade.php
@@ -13,15 +13,10 @@
 <body>
     <div class="pigly__header">
         <h1 class="header__title">PiGLy</h1>
-<<<<<<< HEAD
         <form action="/weight_logs/goal_setting" class="header__form" method="post">
             <a href="/weight_logs/goal_setting" class="change link">目標体重設定</a>
         </form>
             <a href="/logout" class="logout">ログアウト</a>
-=======
-        <a href="/login" class="change link">目標体重設定</a>
-        <a href="/logout" class="logout">ログアウト</a>
->>>>>>> 5892bd1aa6abae8dc313f71a32aef03821ba2dd1
     </div>
     @yield('content')
     
